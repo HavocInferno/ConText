13fa635b16a99a42f7231306c8afdb6e1d4ee32a
Paul Preißner <p.preissner@tum.de>
2016-04-19 09:15:17 +0200

    idea

--- New Unity Project/Assets/Managers/ModuleManager.cs
+++ New Unity Project/Assets/Managers/ModuleManager.cs
@@ -15,4 +15,9 @@ public class ModuleManager : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    /*in UI Manager, functions for adding and hiding messages
+    in here, function for loading module data and initiating it to be added to UI (through UI Mng)
+    and execute whatever is to be executed for the current module
+    what about ones with replies or outcome? module itself should send reply to Module Manager, which then acts upon it.*/
 }
