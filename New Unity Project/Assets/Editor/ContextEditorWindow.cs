7f8796eff87a83fa669b34e7d760932fa0e89301
Paul Preißner (TUM) <paul@preissner-muc.de>
2016-06-20 17:13:58 +0200

    hurr

---
+++ New Unity Project/Assets/Editor/ContextEditorWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ContextEditorWindow : EditorWindow
+{
+    string myString = "This is work in progress";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    [MenuItem("ConText/Project Overview")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        ContextEditorWindow window = (ContextEditorWindow)EditorWindow.GetWindow(typeof(ContextEditorWindow), false, "ConText Overview", true);
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Text Field", myString);
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+        myBool = EditorGUILayout.Toggle("Toggle", myBool);
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
